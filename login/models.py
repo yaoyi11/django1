fb09ae97caaaf09618fc8f4dbef05d31dcd51dc3
yaoyi11 <767248521@qq.com>
2019-02-22 16:12:19 +0800

    spider && sqlite3

--- login/models.py
+++ login/models.py
@@ -1,21 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    gender=(('male', "男"),('female', "女"),)
-    #name必填，最长不超过128个字符，并且唯一，也就是不能有相同姓名；
-    name = models.CharField(max_length=128, unique=True)
-    #password必填，最长不超过256个字符；
-    password = models.CharField(max_length=256)
-    #性别使用了一个choice，只能选择男或者女，默认为男；
-    sex = models.CharField(max_length=32, choices=gender, default="男")
-    #用户创建时间
-    c_time = models.DateTimeField(auto_now_add=True)
-    #使用__str__帮助人性化显示对象信息；
+class Movie(models.Model):
+    name = models.CharField(verbose_name='电影名称', max_length=30)
+    movie_cate = models.CharField(verbose_name='电影分类', max_length=30, blank=True, null=True)
+    movie_img = models.CharField(verbose_name='封面图片', max_length=300, null=True, blank=True)
+    release_date = models.DateField(verbose_name='上映日期')
+    viewed = models.BooleanField(verbose_name='观看状态', default=False)
+    created = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
+
     def __str__(self):
         return self.name
-    #元数据里定义用户按创建时间的反序排列，也就是最近的最先显示；
+
     class Meta:
-        ordering = ["c_time"]
-        verbose_name = "用户"
-        verbose_name_plural = "用户"
+        verbose_name = '电影'
+        verbose_name_plural = verbose_name
+        ordering = ('name',)
